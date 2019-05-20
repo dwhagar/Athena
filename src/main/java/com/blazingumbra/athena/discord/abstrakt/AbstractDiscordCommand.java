25cfcb242b8208a01b4b6e0209c21d0f89b9d8c1
JumpingScript <bekommste.nicht@gmx.de>
2019-05-20 14:19:53 +0200

    Added checkPermission-Method.

--- src/main/java/com/blazingumbra/athena/discord/abstrakt/AbstractDiscordCommand.java
+++ src/main/java/com/blazingumbra/athena/discord/abstrakt/AbstractDiscordCommand.java
@@ -1,5 +1,12 @@
 package com.blazingumbra.athena.discord.abstrakt;
 
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+
 public abstract class AbstractDiscordCommand {
     protected String module;
+
+    public abstract boolean checkPermission(Permission[] permissions, Member member);
+
+    //TODO Implement execute of command
 }
--- src/main/java/com/blazingumbra/athena/discord/commands/admin/AdministratorCommand.java
+++ src/main/java/com/blazingumbra/athena/discord/commands/admin/AdministratorCommand.java
@@ -1,11 +1,23 @@
 package com.blazingumbra.athena.discord.commands.admin;
 
 import com.blazingumbra.athena.discord.abstrakt.AbstractDiscordCommand;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AdministratorCommand extends AbstractDiscordCommand {
     public String command;
 
     AdministratorCommand() {
         module = "admin";
+    }
+
+    @Override
+    public boolean checkPermission(Permission[] permissions, Member member) {
+        List<Permission> memberPermission = member.getPermissions();
+        boolean hasRoles = memberPermission.containsAll(Arrays.asList(permissions));
+        return hasRoles;
     }
 }
