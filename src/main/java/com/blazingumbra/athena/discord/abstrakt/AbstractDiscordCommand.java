49859560aad2de7f274e13ac935cbf16a3cc272b
JumpingScript <bekommste.nicht@gmx.de>
2019-05-23 18:12:09 +0200

    Added logger to AbstractDiscordCommand

--- src/main/java/com/blazingumbra/athena/discord/abstrakt/AbstractDiscordCommand.java
+++ src/main/java/com/blazingumbra/athena/discord/abstrakt/AbstractDiscordCommand.java
@@ -3,11 +3,14 @@ package com.blazingumbra.athena.discord.abstrakt;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractDiscordCommand {
     protected String module;
     protected String command;
     protected String parameters;
+    protected Logger logger = LoggerFactory.getLogger("athena.command");
 
     public abstract boolean checkPermission(Permission[] permissions, Member member);
 
