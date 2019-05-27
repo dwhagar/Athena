f52560c29ca915158cc5d8631d32e99c5e3ae7e2
JumpingScript <bekommste.nicht@gmx.de>
2019-05-27 11:28:06 +0200

    Moved implementation from WikiInteractor#retrieveLoginToken to GetLoginTokenWikiInteraction#execute, removed duplicate class.

--- src/main/java/com/blazingumbra/athena/wiki/interaction/GetPageTextInteraction.java
+++
@@ -1,8 +0,0 @@
-package com.blazingumbra.athena.wiki.interaction;
-
-public class GetPageTextInteraction {
-    public static void execute(WikiSession aWikiSession, String parameter)
-    {
-
-    }
-}
