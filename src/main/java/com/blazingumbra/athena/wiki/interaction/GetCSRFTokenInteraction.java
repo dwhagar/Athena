be8f456c293f4e7f146c4691d12e7d579e32038f
JumpingScript <bekommste.nicht@gmx.de>
2019-05-27 11:30:03 +0200

    Moved implementation from WikiInteractor#retrieveCSRFToken to GetCSRFTokenWikiInteraction#execute

--- src/main/java/com/blazingumbra/athena/wiki/interaction/GetCSRFTokenInteraction.java
+++ src/main/java/com/blazingumbra/athena/wiki/interaction/GetCSRFTokenInteraction.java
@@ -1,8 +1,25 @@
 package com.blazingumbra.athena.wiki.interaction;
 
-public class GetCSRFTokenInteraction {
-    public static void execute(WikiSession aWikiSession, String parameter)
-    {
+import com.blazingumbra.athena.wiki.objects.CsrfTokenResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class GetCSRFTokenInteraction extends AbstractWikiInteraction{
+    public static CsrfTokenResponse execute(WikiSession aWikiSession, String parameter) throws IOException, URISyntaxException {
+        String s;
+        URIBuilder uriBuilder = new URIBuilder(aWikiSession.getBaseURI());
+        URI anURI = uriBuilder
+                .addParameter("action", "query")
+                .addParameter("format", "json")
+                .addParameter("meta", "tokens")
+                .addParameter("type", "csrf")
+                .build();
+        HttpGet get = new HttpGet(anURI);
+        s = aWikiSession.getResult(get);
+        return gson.fromJson(s, CsrfTokenResponse.class);
     }
 }
